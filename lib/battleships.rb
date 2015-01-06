e6755b485419b481fb8ed0ae4efe889b29c91a15
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-01-06 16:58:02 +0000

    added ability to post names

--- lib/battleships.rb
+++ lib/battleships.rb
@@ -10,7 +10,15 @@ class BattleShips < Sinatra::Base
 
   get '/new_game' do 
     erb :new_game
-  end 
+  end
+
+  post '/form' do 
+    @player1 = params[:player1]
+    @player2 = params[:player2]
+    erb :new_game
+  end
+
+
 
   # start the server if ruby file executed directly
   run! if app_file == $0
