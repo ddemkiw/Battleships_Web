2cf56b49d8b7b8cdca8a06cc4464111aa2b64621
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-01-06 16:02:49 +0000

    Defined views folder

--- lib/battleships.rb
+++ lib/battleships.rb
@@ -2,19 +2,11 @@ require 'sinatra/base'
 
 class BattleShips < Sinatra::Base
 
-   get '/' do
-    'Hello BattleShips!'
+  set :views, File.expand_path('../../views', __FILE__)
+
+  get '/' do
+    erb :index
   end
-
-  # get '/' do
-  #   erb :index
-  # end
-
-  # get '/new_game' do
-  #   erb :new_game
-  # end
-
-
 
   # start the server if ruby file executed directly
   run! if app_file == $0
