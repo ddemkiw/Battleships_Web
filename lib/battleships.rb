6c05e7de1acb0fcf6c5985a9781be24d9009251e
Danielle Demkiw <ddemkiw@gmail.com>
2015-01-07 13:43:20 +0000

    added set up method and changed variable names for players

--- lib/battleships.rb
+++ lib/battleships.rb
@@ -1,7 +1,9 @@
 require 'sinatra/base'
 require 'game'
 require 'player'
-
+require 'ship'
+require 'board'
+require 'cell'
 
 class BattleShips < Sinatra::Base
 
@@ -14,23 +16,34 @@ class BattleShips < Sinatra::Base
   end
 
   get '/new_game' do 
-    @game = Game.new
     erb :new_game
   end
 
   post '/form' do 
-    @player1 = params[:player1]
-    @player2 = params[:player2]
+    @player1_name = params[:player1]
+    @player2_name = params[:player2]
     erb :new_game
   end
 
   get '/setup_game' do 
-    @player1 = Player.new
-    @player2 = Player.new
+    @game = Game.new
+    setup(@player1, @player1_name)
+    setup(@player2, @player2_name)
     erb :setup_game
   end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
+
+  private
+
+    def setup(player, name)
+      player = Player.new
+      player.name = name
+      @fleet1 = [Ship.aircraft_carrier, Ship.battleship, Ship.destroyer, Ship.submarine, Ship.patrol_boat ]
+      @board1 = Board.new(Cell)
+      @game.add_player(player)
+      player.board = @board1
+    end 
 
 end
